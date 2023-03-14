8671d624f8b9029bc88d5aee73f5d8e30dedd928
astudentinearth <95227070+astudentinearth@users.noreply.github.com>
2023-03-15 02:00:39 +0300

    Change drag-drop library to dnd-kit in task panel

--- src/UI/Components/TaskItem.tsx
+++ src/UI/Components/TaskItem.tsx
@@ -3,17 +3,18 @@ import { useDrop,useDrag } from "react-dnd"
 import { Identifier, XYCoord } from "dnd-core"
 import { DraggableTypes } from "../../Util"
 import { SaveTasks, TasksContext } from "../Tasks"
+import { useSortable } from "@dnd-kit/sortable"
+import {CSS} from "@dnd-kit/utilities"
 
 export interface TaskItemProps{
     id: string
     content: string
     completed: boolean
     index: number
-    reorder: (dragIndex: number, hoverIndex: number) => void
     type: string
 }
 
-export const TaskItem: FC<TaskItemProps> = ({id,content,completed,index,reorder,type})=>{
+/*export const TaskItem: FC<TaskItemProps> = ({id,content,completed,index,reorder,type})=>{
     const ref = useRef<HTMLDivElement>(null);
     const {tasks, setTasks} = useContext(TasksContext);
     const [{handlerId},drop] = useDrop<
@@ -72,5 +73,42 @@ export const TaskItem: FC<TaskItemProps> = ({id,content,completed,index,reorder,
             setTasks(currentTasks);
             SaveTasks();
         }} className="task-delete-button ml-auto mr-4 hover:bg-secondary/20 cursor-pointer rounded-md p-1 w-6 h-6 flex items-center justify-center"><i className="bi-x-lg"></i></div>
+    </div>
+}*/
+
+
+export function TaskItem(props:TaskItemProps){
+    const {attributes, listeners, setNodeRef, transform, transition} = useSortable({id: props.id});
+    const {tasks, setTasks} = useContext(TasksContext);
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition
+    }
+    return <div ref={setNodeRef} style={style} {...attributes} {...listeners}
+    className="task-item transition-colors">
+        <div onClick={()=>{
+            let currentTasks = [...tasks];
+            for (let t of currentTasks){
+                if(t.id!==props.id) continue;
+                t.completed = t.completed ? false : true;
+            }
+            setTasks(currentTasks);
+            SaveTasks();
+        }}
+        style={props.completed ? {background: "rgb(var(--accent))"} : {}} className="w-5 h-5 ml-2 flex items-center justify-center rounded-md bg-secondary/75 hover:brigtness-125 cursor-pointer checkbox checkbox-shadow">
+            {props.completed ? <i className="bi-check-lg text-white checkbox"></i> : ""}
+        </div>
+        <span className="text-default p-2 text-md">{props.content}</span>
+        <div onClick={(event)=>{
+            event?.stopPropagation();
+            let currentTasks=[...tasks];
+            for (let t in currentTasks){
+               if(currentTasks[t].id===props.id){
+                    currentTasks.splice(parseInt(t),1);
+               } 
+            }
+            setTasks(currentTasks);
+            SaveTasks();
+        }} className="task-delete-button ignore-drag ml-auto mr-4 hover:bg-secondary/20 cursor-pointer rounded-md p-1 w-6 h-6 flex items-center justify-center"><i className="bi-x-lg ignore-drag"></i></div>
     </div>
 }
