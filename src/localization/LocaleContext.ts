83624ebda860a5e516d145b545e4808773e54593
astudentinearth <95227070+astudentinearth@users.noreply.github.com>
2023-05-27 21:52:29 +0300

    Remove code for catspeak

--- src/localization/LocaleContext.ts
+++ src/localization/LocaleContext.ts
@@ -1,7 +1,6 @@
 import React, { Dispatch, SetStateAction } from "react";
 import en_us from './locales/en_us.json'
 import tr_tr from './locales/tr_tr.json'
-import catspeak from './locales/catspeak.json'
 
 export interface LocaleContextType{
     locale: string,
@@ -17,9 +16,6 @@ export function GetLocalizedResource(resourceName: string, locale: string){
 
         case "tr_tr":
             return tr_tr[resourceName];
-    
-        case "catspeak":
-            return catspeak[resourceName];
     
     }
     return en_us[resourceName];
