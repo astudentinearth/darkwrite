e92816d49593efe2b88328f25e087e76d9d40a16
Burak <burak.yn.dev@gmail.com>
2024-06-28 02:48:04 +0300

    ➕ Add dependencies and update path resolution - Setup shadcn-ui and tailwindcss - Alias @ to /src

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,11 @@ export default defineConfig({
     target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     sourcemap: !!process.env.TAURI_DEBUG,
-  }
+  },
+  resolve:{
+    alias:{
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+
 })
