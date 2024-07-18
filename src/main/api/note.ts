edf075d3aa3daf84fbbb3ec4b6eb65116a1fc9e2
Burak <burak.yn.dev@gmail.com>
2024-07-19 00:56:46 +0300

    🐛 Notes now have a default icon

--- src/main/api/note.ts
+++ src/main/api/note.ts
@@ -25,7 +25,7 @@ export async function createNote(title: string, parent?: string){
         note.isFavorite = false;
         note.isTrashed = false;
         note.subnotes = [];
-        note.icon = "";
+        note.icon = ":page_facing_up:";
         note.id = randomUUID();
         const filename = join(notesDir, `${note.id}.json`);
         await fse.ensureFile(filename);
