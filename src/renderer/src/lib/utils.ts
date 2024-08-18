2018a21cecd853aedf7a481b9d88d25baa894327
Burak <burak.yn.dev@gmail.com>
2024-08-19 02:22:10 +0300

    🚧 Add an unicode converter for emojis

--- src/renderer/src/lib/utils.ts
+++ src/renderer/src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function fromUnicode(unicode: string){
+  const points = unicode.split("-").map((p)=>parseInt(p, 16));
+  return String.fromCodePoint(...points);
+}
