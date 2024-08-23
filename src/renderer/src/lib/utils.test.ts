3ff410379e1c62acd1ae9df45113154f648237d1
Burak <burak.yn.dev@gmail.com>
2024-08-23 18:47:05 +0300

    ✅ Add test for unicode converter

---
+++ src/renderer/src/lib/utils.test.ts
@@ -0,0 +1,12 @@
+import { fromUnicode } from "./utils";
+
+it("should convert emojis correctly", ()=>{
+    const emojis = {
+        "1f9d1-200d-1f4bb": "🧑‍💻",
+        "1f636-200d-1f32b-fe0f": "😶‍🌫️",
+        "1f97d": "🥽"
+    }
+    expect(fromUnicode("1f9d1-200d-1f4bb")).toBe(emojis["1f9d1-200d-1f4bb"]);
+    expect(fromUnicode("1f636-200d-1f32b-fe0f")).toBe(emojis["1f636-200d-1f32b-fe0f"]);
+    expect(fromUnicode("1f97d")).toBe(emojis["1f97d"]);
+})
