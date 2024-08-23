6c76185294707e34bfbb76a2f3dd4adbbfb59551
Burak <burak.yn.dev@gmail.com>
2024-08-23 18:35:00 +0300

    🐛 Fix code point error in emoji conversion

--- src/renderer/src/lib/utils.ts
+++ src/renderer/src/lib/utils.ts
@@ -8,5 +8,5 @@ export function cn(...inputs: ClassValue[]) {
 export function fromUnicode(unicode: string){
   if(unicode == null) return "";
   const points = unicode.split("-").map((p)=>parseInt(p, 16));
-  return String.fromCodePoint(...points);
+  return String.fromCodePoint(...points.filter((p)=>!isNaN(p)));
 }
