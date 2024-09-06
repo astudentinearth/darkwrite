3136878befbbefd6882323499717effbae8ef3bf
Burak <burak.yn.dev@gmail.com>
2024-09-07 00:37:33 +0300

    🚧 Scaffold note customization UI

--- packages/app-desktop/src/renderer/src/components/ui/button.tsx
+++ packages/app-desktop/src/renderer/src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border dw-border bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-secondary hover:text-secondary-foreground active:bg-secondary/80",
--- packages/app-desktop/src/renderer/src/features/editor/customization-sheet.tsx
+++ packages/app-desktop/src/renderer/src/features/editor/customization-sheet.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@renderer/components/ui/button";
+import { Input } from "@renderer/components/ui/input";
+import { Label } from "@renderer/components/ui/label";
 import { Sheet, SheetContent, SheetTitle } from "@renderer/components/ui/sheet";
+import { Switch } from "@renderer/components/ui/switch";
+import { RotateCcw } from "lucide-react";
 
 export function CustimzationSheet(props: {open: boolean, onOpenChange: (boolean)=>void}){
     return <Sheet open={props.open} onOpenChange={props.onOpenChange}>
-        <SheetContent className="pt-10 flex flex-col gap-2">
+        <SheetContent className="pt-10 flex flex-col gap-4">
             <SheetTitle className="text-2xl">Customize</SheetTitle>
             {/* Style chooser */}
             <div className="rounded-2xl bg-view-1 p-1 ">
@@ -33,8 +37,28 @@ export function CustimzationSheet(props: {open: boolean, onOpenChange: (boolean)
                         </div>
                     </Button>
                 </div>
-                <div className="p-1">
-                    
+                <div className="p-1 flex flex-col gap-4">
+                    <div className="flex items-center gap-2">
+                        <Input className="bg-secondary/25 rounded-xl" id="customFont" placeholder="Custom font name"/>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <Switch id="largeText"/>
+                        <Label htmlFor="largeText">Use large text</Label>
+                    </div>
+                </div>
+            </div>
+            <div className="rounded-2xl bg-view-1 p-2 flex flex-col gap-2">
+                <div className="flex items-center gap-1">
+                    <span className="flex-shrink-0 font-medium">Background color</span>
+                    <div className="w-full"></div>
+                    <Input placeholder="Default" className="h-8 w-24"/>
+                    <Button className="flex-shrink-0 hover:bg-secondary/50" size={"icon32"} variant={"outline"}><RotateCcw size={18} className="opacity-75"/></Button>
+                </div>
+                <div className="flex items-center gap-1">
+                    <span className="flex-shrink-0 font-medium">Default text color</span>
+                    <div className="w-full"></div>
+                    <Input placeholder="Default" className="h-8 w-24"/>
+                    <Button className="flex-shrink-0 hover:bg-secondary/50" size={"icon32"} variant={"outline"}><RotateCcw size={18} className="opacity-75"/></Button>
                 </div>
             </div>
         </SheetContent>
