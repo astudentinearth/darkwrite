b2366004fa3adbfa149b55fca11e720582944244
Burak <burak.yn.dev@gmail.com>
2025-03-17 20:41:15 +0300

    📱 Make docs page responsive

--- packages/website/src/app/docs/[slug]/page.tsx
+++ packages/website/src/app/docs/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { DocsSidebar } from "@/components/docs-sidebar";
+import { DocsSidebar, DocsSidebarSheet } from "@/components/docs-sidebar";
 import { Docs } from "@/docs";
 import { notFound } from "next/navigation";
 import "./docs.css";
@@ -11,14 +11,20 @@ export default async function DocsPage({
   params: Promise<{ slug: string }>;
 }) {
   const slug = (await params).slug;
-  if(!(paths as string[]).includes(slug)) return notFound();
+  if (!(paths as string[]).includes(slug)) return notFound();
 
   return (
     <div className="absolute w-full h-full flex overflow-x-hidden">
-      <DocsSidebar />
-      <div className="grow flex bg-muted/70 justify-center">
-        <div className="markdown-container px-8 h-full overflow-auto w-full max-w-240">
+      <DocsSidebar className="hidden md:flex" />
+      <div className="grow flex flex-col bg-muted/30 md:m-2 md:rounded-xl md:border md:border-border drop-shadow-lg">
+        <div className="bg-background z-50 p-2 rounded-xl m-2 border border-border flex items-center gap-2 md:hidden">
+          <DocsSidebarSheet />
+          <span className="opacity-80">Docs</span>
+        </div>
+        <div className="markdown-container px-8 h-full overflow-auto w-full flex justify-center">
+          <div className="max-w-240">
           {Docs[slug as keyof typeof Docs]}
+          </div>
         </div>
       </div>
     </div>
@@ -26,5 +32,5 @@ export default async function DocsPage({
 }
 
 export function generateStaticParams() {
-  return paths.map((slug)=>({slug}));
+  return paths.map((slug) => ({ slug }));
 }
--- packages/website/src/components/docs-sidebar.tsx
+++ packages/website/src/components/docs-sidebar.tsx
@@ -3,41 +3,60 @@
 import Link from "next/link";
 import { DocumentationLink } from "./docs-link";
 import Image from "next/image";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Info, Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { cn } from "@/lib/utils";
 
-export function DocsSidebar() {
+export function DocsSidebar({className}: {className?: string}) {
   return (
-    <div className=" h-full w-72 flex flex-col p-2 shrink-0">
+    <div className={cn("h-full w-72 flex flex-col p-2 shrink-0", className)}>
       <div className="text-xl shrink-0 font-semibold p-2 pb-4 flex items-center gap-2">
         <Image src={"/darkwrite_icon.png"} alt="logo" width={32} height={32} />
         <span>Docs</span>
       </div>
+      <span className="block p-3 bg-primary/30 rounded-xl text-sm mb-2 text-muted-foreground">
+        <Info size={16} className="inline" /> This documentation is a
+        work-in-progress.
+      </span>
       <DocumentationLink href={"/docs/install"}>
         ⬇️ &nbsp;Installation
       </DocumentationLink>
       <DocumentationLink href={"/docs/backup"}>
         📦 &nbsp;Backup and restore
       </DocumentationLink>
-      <DocumentationLink href={"/docs/creating-themes"}>
+      {/* <DocumentationLink href={"/docs/creating-themes"}>
         🎨 &nbsp;Create your own theme
-      </DocumentationLink>
-      <DocumentationLink href={"/docs/customization"}>
+      </DocumentationLink> */}
+      {/* <DocumentationLink href={"/docs/customization"}>
         🖌️ &nbsp;Customization
-      </DocumentationLink>
+      </DocumentationLink> */}
       <DocumentationLink href={"/docs/workspace-export"}>
         📃 &nbsp;Export your workspace
       </DocumentationLink>
-      <DocumentationLink href={"/docs/shortcuts"}>
+      {/* <DocumentationLink href={"/docs/shortcuts"}>
         ⌨️ &nbsp;Keyboard shortcuts
-      </DocumentationLink>
+      </DocumentationLink> */}
       <div className="grow"></div>
       <Link
         className="p-2 rounded-[8px] flex gap-2 items-center shrink-0 hover:bg-muted/30 cursor-default"
         href={"/"}
       >
-        <ArrowLeft />
+        <ArrowLeft size={20} />
         Return to home page
       </Link>
     </div>
+  );
+}
+
+export function DocsSidebarSheet() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="opacity-80" size={20}/>
+      </SheetTrigger>
+      <SheetContent className="w-fit border-border" side="left">
+        <DocsSidebar />
+      </SheetContent>
+    </Sheet>
   );
 }
