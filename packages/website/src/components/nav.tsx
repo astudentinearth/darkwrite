2eeff9a1c2e92da464bb42b392a6a696d29b09db
Burak <burak.yn.dev@gmail.com>
2025-03-18 19:59:54 +0300

    📱 Make website navigation responsive

--- packages/website/src/components/nav.tsx
+++ packages/website/src/components/nav.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
+import ResourcesPopover from "./resources-popover";
+import MenuPopover from "./menu";
 
 export async function Nav() {
   return (
-    <div className="fixed w-full flex justify-center top-3 z-50">
-      <nav className="bg-nav/80 backdrop-blur-md justify-center min-w-0 w-fit max-w-max p-3 border border-[#c1c1c1]/25 rounded-[18px] flex gap-3">
-        <Link href={"/"} className="shrink-0">
+    <div className="fixed w-full flex justify-center px-3 top-3 z-50">
+      <nav className="bg-nav/80 backdrop-blur-md justify-start xs:justify-center min-w-0 grow xs:w-fit xs:max-w-max p-3 border border-[#c1c1c1]/25 rounded-[18px] flex gap-2">
+        <Link href={"/"} className="shrink-0 xs:mr-2">
           <Image
             src={"/darkwrite_icon.png"}
             alt="Logo"
@@ -13,18 +15,15 @@ export async function Nav() {
             height={40}
           />
         </Link>
-        <Link
-          href={"/docs/install"}
-          className="h-10 flex shrink-0 justify-center items-center p-3 select-none hover:bg-white/10 transition-colors rounded-[6px]"
-        >
-          User guide
-        </Link>
+        <ResourcesPopover/>
         <Link
           href={"https://github.com/astudentinearth/darkwrite/releases"}
-          className="bg-primary shrink-0 h-10 p-3 flex items-center justify-center rounded-[6px] hover:brightness-125 transition-[filter]"
+          className="bg-primary shrink-0 h-10 p-3 hidden xs:flex items-center justify-center rounded-[6px] hover:brightness-125 transition-[filter] ml-2"
         >
           Download
         </Link>
+        <div className="grow xs:hidden"></div>
+        <MenuPopover/>
       </nav>
     </div>
   );
