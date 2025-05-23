aec3d2247c055733ea508b141f6b31f30842f164
Burak <burak.yn.dev@gmail.com>
2025-05-23 19:15:14 +0300

    🚧 Bring back the editor context menu

--- packages/app-desktop/src/features/editorv2/editor.tsx
+++ packages/app-desktop/src/features/editorv2/editor.tsx
@@ -1,7 +1,15 @@
 import { DarkwriteEditor, DarkwriteEditorProps } from "@darkwrite/editor";
+import { ContextMenu, ContextMenuTrigger } from "@darkwrite/ui";
+import { EditorContextMenuContent } from "../editor/context-menu";
 
 export default function DarkwriteEditorView(props: DarkwriteEditorProps) {
   return (
-   <DarkwriteEditor {...props}/> 
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <DarkwriteEditor {...props}/> 
+      </ContextMenuTrigger>
+      <EditorContextMenuContent/>
+    </ContextMenu>
+   
   )
 }
