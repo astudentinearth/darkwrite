e92588fce8ec4e47801b73d96cc5f77919d1cf9c
Burak <burak.yn.dev@gmail.com>
2025-05-27 02:00:09 +0300

    🎉 Begin archiectural revamp Create new settings schema

---
+++ packages/common/src/models/settings.ts
@@ -0,0 +1,64 @@
+import _ from "lodash";
+
+export type ThemeMode = "light" | "dark" | "system";
+
+export const DEFAULT_THEME_SETTINGS = {
+  themeMode: "dark" as ThemeMode,
+  lightColorScheme: "darkwrite-catppuccin-latte" as string,
+  darkColorScheme: "darkwrite-default" as string,
+  accentColor: "#302e70" as string,
+  useSystemWindowFrame: false as boolean,
+  fonts: {
+    sans: "Inter, Helvetica, Arial, sans-serif" as string,
+    serif: "Times New Roman, serif" as string,
+    code: "JetBrains Mono, Cascadia Code, Noto Mono, monospace" as string,
+    ui: "SF Pro Display, Inter, Segoe UI, Noto Sans, Cantarell, system-ui" as string
+  },
+  experimental: {
+    darwinCustomTitlebarEnabled: false as boolean
+  },
+  customCSS: "" as string
+};
+
+export const DEFAULT_EDITOR_SETTINGS = {
+  spellcheckerEnabled: true as boolean,
+  codeIndentSize: 4 as number,
+  wordCountHudEnabled: false as boolean,
+  disabledCommandItems: [] as string[]
+};
+
+export const DEFAULT_CLIENT_SETTINGS = {
+  autoUpdateCheck: false as boolean,
+  language: "en" as string
+}
+
+export type ThemeSettings = typeof DEFAULT_THEME_SETTINGS;
+export type EditorSettings = typeof DEFAULT_EDITOR_SETTINGS;
+export type ClientSettings = typeof DEFAULT_CLIENT_SETTINGS;
+
+export type DarkwriteUserSettings = {
+  appearance: ThemeSettings,
+  editor: EditorSettings,
+  client: ClientSettings,
+  version: 1
+}
+
+export class SettingsModel {
+
+  public static getDefaults(): DarkwriteUserSettings{
+    return _.cloneDeep({
+      appearance: DEFAULT_THEME_SETTINGS,
+      client: DEFAULT_CLIENT_SETTINGS,
+      editor: DEFAULT_EDITOR_SETTINGS,
+      version: 1
+    });
+  }
+
+  public static mergeWith(settings: Partial<DarkwriteUserSettings>) {
+    const defaults = this.getDefaults();
+    const partial = _.cloneDeep(settings);
+    _.merge(partial, defaults);
+    return partial as DarkwriteUserSettings;
+  }
+
+}
