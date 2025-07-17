ee5f22bc3f40af68503cc3def8cb014678f1aee0
Burak Yeniçeri <burak.yn.dev@gmail.com>
2025-07-17 22:39:02 +0300

    🚧 Add repositories

---
+++ app/src/electron/repository/database.repository.ts
@@ -0,0 +1,35 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../db";
+import { Database } from "../entity";
+
+export class NoteRepository {
+  private _repo: Repository<Database>;
+  constructor(private _datasource = AppDataSource) {
+    this._repo = _datasource.getRepository(Database);
+  }
+
+  async save(database: Database) {
+    return this._repo.save(database);
+  }
+
+  async saveAll(databases: Database[]) {
+    return this._repo.save(databases);
+  }
+
+  async findById(id: string) {
+    return this._repo.findOne({where: {id}});
+  }
+
+  async findAll() {
+    return this._repo.findBy({});
+  }
+
+  async deleteById(id: string) {
+    return this._repo.delete({id});
+  }
+
+  async delete(database: Database) {
+    return this._repo.delete({id: database.id});
+  }
+
+}
---
+++ app/src/electron/repository/note.repository.ts
@@ -0,0 +1,43 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../db";
+import { Note } from "../entity";
+
+export class NoteRepository {
+  private _repo: Repository<Note>;
+  constructor(private _datasource = AppDataSource) {
+    this._repo = _datasource.getRepository(Note);
+  }
+
+  async save(note: Note) {
+    return this._repo.save(note);
+  }
+
+  async saveAll(notes: Note[]) {
+    return this._repo.save(notes);
+  }
+
+  async findById(id: string) {
+    return this._repo.findOne({where: {id}});
+  }
+
+  async findAll() {
+    return this._repo.find();
+  }
+
+  async findAllByWorkspaceId(workspaceId: string) {
+    return this._repo.findBy({workspace: {id: workspaceId}});
+  }
+
+  async findAllByDatabaseId(databaseId: string) {
+    return this._repo.findBy({database: {id: databaseId}});
+  }
+
+  async deleteById(id: string) {
+    return this._repo.delete({id});
+  }
+
+  async delete(note: Note) {
+    return this._repo.delete({id: note.id});
+  }
+
+}
