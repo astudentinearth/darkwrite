f066941a4c305b7f468e46108f285a75c0211fa7
Burak Yeniçeri <burak.yn.dev@gmail.com>
2025-10-13 01:40:47 +0300

    🐛Fix z-index issue in editor

--- app/src/features/editor/header.tsx
+++ app/src/features/editor/header.tsx
@@ -24,45 +24,80 @@ export default function EditorHeader(props: EditorHeaderProps) {
   const { t } = useTranslation();
   const mouseOver = useMouseOver();
   return (
-    <div className={cn("w-full flex flex-col items-center pt-24", props.coverImageSource && "pt-48")} {...mouseOver.hoverProps}>
+    <div
+      className={cn(
+        "w-full flex flex-col items-center pt-24",
+        props.coverImageSource && "pt-48",
+      )}
+      {...mouseOver.hoverProps}
+    >
       <CoverImage
         onImageSourceChange={props.onCoverSourceChange}
         imageSource={props.coverImageSource}
         onAddCover={props.onAddCover}
       />
-      <ConstrainedWidth className={cn("flex flex-col gap-2 px-4")} fill={props.wide}>
+      <ConstrainedWidth
+        className={cn("flex flex-col gap-2 px-4")}
+        fill={props.wide}
+      >
         <div className="flex gap-2 items-end mb-4 font-ui">
-          {props.icon &&
+          {props.icon && (
             <EmojiPicker
               show={fromUnicode(props.icon ?? "")}
               closeOnSelect
               onSelect={props.onIconChange}
-              className={cn("z-50 -translate-x-2", props.coverImageSource && "-mt-8")}
+              className={cn(
+                "z-50 -translate-x-2",
+                props.coverImageSource && "-mt-8",
+              )}
             />
-          }
-          <div className={cn("opacity-0 z-20 font-ui", mouseOver.mouseOver && "opacity-100")}>
-            {!props.icon &&
+          )}
+          <div
+            className={cn(
+              "opacity-0 z-20 font-ui",
+              mouseOver.mouseOver && "opacity-100",
+            )}
+          >
+            {!props.icon && (
               <>
                 <Button
                   onClick={() => props.onIconChange("1f4c4")}
-                  className="w-fit" variant={"ghost"}><Smile size={18} />{t("editor.cover.addIcon")}</Button>
-
+                  className="w-fit"
+                  variant={"ghost"}
+                >
+                  <Smile size={18} />
+                  {t("editor.cover.addIcon")}
+                </Button>
               </>
-            }
-            {props.icon &&
+            )}
+            {props.icon && (
               <Button
                 onClick={() => props.onIconChange(null)}
-                className="w-fit" variant={"ghost"}><Frown size={18} />{t("editor.cover.removeIcon")}</Button>
-            }
-            {!props.coverImageSource &&
+                className="w-fit"
+                variant={"ghost"}
+              >
+                <Frown size={18} />
+                {t("editor.cover.removeIcon")}
+              </Button>
+            )}
+            {!props.coverImageSource && (
               <Button
                 onClick={props.onAddCover}
-                className="w-fit" variant={"ghost"}><Image size={18} />{t("editor.cover.addCover")}</Button>
-            }
+                className="w-fit"
+                variant={"ghost"}
+              >
+                <Image size={18} />
+                {t("editor.cover.addCover")}
+              </Button>
+            )}
           </div>
         </div>
 
-        <DynamicTextarea className="text-4xl z-20 font-semibold box-border h-auto overflow-hidden resize-none grow outline-hidden block" defaultValue={props.title} onValueChange={props.onTitleChange} />
+        <DynamicTextarea
+          className="text-4xl font-semibold box-border h-auto overflow-hidden resize-none grow outline-hidden block"
+          defaultValue={props.title}
+          onValueChange={props.onTitleChange}
+        />
         <hr />
       </ConstrainedWidth>
     </div>
