67d503cb15b27a770551bb860cbf12e652927784
Burak Yeniçeri <burak.yn.dev@gmail.com>
2025-10-20 00:04:01 +0300

    🚧Downloads page, 3rd party licenses

---
+++ website/src/lib/resources.ts
@@ -0,0 +1,13 @@
+// This is a temporary measure, next release will use named artifacts
+export const WINDOWS_LATEST_RELEASE =
+  "https://github.com/astudentinearth/darkwrite/releases/download/v0.5.3-alpha.2/Darkwrite-Windows-0.5.3-alpha.2-Setup.exe";
+export const LINUX_LATEST_RELEASE =
+  "https://github.com/astudentinearth/darkwrite/releases/download/v0.5.3-alpha.2/Darkwrite-Linux-0.5.3-alpha.2.AppImage";
+export const MACOS_LATEST_RELEASE =
+  "https://github.com/astudentinearth/darkwrite/releases/download/v0.5.3-alpha.2/Darkwrite-Mac-0.5.3-alpha.2-Installer.dmg";
+
+export const AUR_PACKAGE_URL =
+  "https://aur.archlinux.org/packages/darkwrite-bin";
+
+export const GIT_REPO_URL = "https://github.com/astudentinearth/darkwrite";
+export const RELEASE_PAGE_URL = `${GIT_REPO_URL}/releases`;
