f1eece8d31daa1c2277cc33ffa2ce6a3fd5fbe25
Burak Yeniçeri <burak.yn.dev@gmail.com>
2025-11-28 20:23:16 +0300

    fix: navigate back to home page when current note is deleted

--- app/src/query/use-delete-note.ts
+++ app/src/query/use-delete-note.ts
@@ -1,16 +1,23 @@
 import { DarkwriteAPIClient } from "@/api/api-client";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useCurrentWorkspace } from "./use-workspace";
+import { useNoteFromURL } from "./use-note-from-url";
+import { useNavigate } from "react-router-dom";
 
 export default function useDeleteNote() {
   const qc = useQueryClient();
   const workspace = useCurrentWorkspace();
+  const currentNoteId = useNoteFromURL();
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: async (id: string) => {
       DarkwriteAPIClient.note.delete(id);
     },
-    onSuccess() {
-      qc.invalidateQueries({queryKey: [workspace?.id, "notes"]})
-    }
+    onSuccess(data, variables) {
+      if (currentNoteId == variables) {
+        navigate("/");
+      }
+      qc.invalidateQueries({ queryKey: [workspace?.id, "notes"] });
+    },
   });
 }
