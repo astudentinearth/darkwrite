f2224ef9e8c8bb162f9e723dbf26c89127e5cc5a
Burak Yeniçeri <burak.yn.dev@gmail.com>
2026-03-02 23:44:43 +0300

    fix: remove tanstack query

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,4 @@
 import { Layout } from "@/features/layout";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { EditorViewRouteHandler } from "./features/editor/editor-view";
 import HomePage from "./features/home/home-page";
@@ -10,27 +8,24 @@ import { Provider } from "react-redux";
 import { store } from "./features/store/redux";
 
 function App() {
-  const [queryClient] = useState(() => new QueryClient());
   return (
     <div className="w-full h-full overflow-hidden app-fade-in">
-      <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <HashRouter>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route index element={<HomePage />}></Route>
-                <Route
-                  path="/page/:pageId"
-                  loader={noteLoader}
-                  element={<EditorViewRouteHandler />}
-                ></Route>
-                <Route path="settings" element={<></>}></Route>
-              </Route>
-            </Routes>
-          </HashRouter>
-          <UpdateChecker />
-        </Provider>
-      </QueryClientProvider>
+      <Provider store={store}>
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />}></Route>
+              <Route
+                path="/page/:pageId"
+                loader={noteLoader}
+                element={<EditorViewRouteHandler />}
+              ></Route>
+              <Route path="settings" element={<></>}></Route>
+            </Route>
+          </Routes>
+        </HashRouter>
+        <UpdateChecker />
+      </Provider>
     </div>
   );
 }
