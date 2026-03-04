457e1d3f4ae866d0d25495ac6a86091d6eeb04b5
Burak Yeniçeri <burak.yn.dev@gmail.com>
2026-03-05 02:43:46 +0300

    feat: notarize mac builds (hopefully)

---
+++ app/notarize.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { notarize } from "@electron/notarize";
+
+export default async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== "darwin") {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    teamId: process.env.APPLE_TEAM_ID,
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+  });
+}
