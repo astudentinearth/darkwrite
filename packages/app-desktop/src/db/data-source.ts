ebcf42f8a0e72302b264e2e80d89b45ae2047f1a
Burak Yeniçeri <burak.yn.dev@gmail.com>
2026-03-12 00:45:14 +0300

    add drizzle-orm dependencies
    rebase desktop/drizzle-orm to dev, resolve lockfile conflicts

--- packages/app-desktop/src/db/data-source.ts
+++ packages/app-desktop/src/db/data-source.ts
@@ -2,11 +2,17 @@ import { DataSource } from "typeorm";
 import { DB_PATH } from "../lib/paths";
 import * as entities from "@/entity";
 
+import Database from "better-sqlite3";
+import {drizzle} from "drizzle-orm/better-sqlite3";
+
 const dbPath = DB_PATH;
 
 export const AppDataSource = new DataSource({
   type: "better-sqlite3",
   database: process.env["NODE_ENV"] === "test" ? ":memory:" : dbPath,
   entities,
-  synchronize: true, // FIXME: REMOVE BEFORE RELEASE
+  synchronize: true, // FIXME: ~~REMOVE BEFORE RELEASE~~ lol i'm removing typeorm
 });
+
+export const drizzleDb = drizzle(new Database(dbPath));
+
