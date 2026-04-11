ce1e949c8bac09303efd94bb6239705a58c05ed1
Burak Yeniçeri <burak.yn.dev@gmail.com>
2026-04-11 17:36:29 +0300

    setup sitemap

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://darkwrite.app",
+  integrations: [sitemap()],
 });
