c8d807b8f6f23f9f8b1c729dc4638b0d8cee5b01
Burak Yeniçeri <burak.yn.dev@gmail.com>
2026-04-20 21:00:34 +0300

    fix(ui): new update notification style

--- packages/frontend/src/features/notifications/update-toast.tsx
+++ packages/frontend/src/features/notifications/update-toast.tsx
@@ -1,5 +1,6 @@
-import { ArrowUpRightFromSquare } from "lucide-react";
+import { ArrowUpRightFromSquare, Download } from "lucide-react";
 import { useTranslation } from "react-i18next";
+import ToastContainer from "./components/base-toast";
 
 interface UpdateToastProps {
   version: string;
@@ -9,16 +10,21 @@ interface UpdateToastProps {
 export default function UpdateToast({ version, href }: UpdateToastProps) {
   const { t } = useTranslation();
   return (
-    <div className="flex w-full justify-between items-center">
-      {t("toast.update.description", { version })}
+    <ToastContainer className="relative text-star items-center rounded-2xl gap-2 w-108 flex border-star/50">
+      <div className="absolute inset-0 rounded-[inherit] shrink-0 pointer-events-none bg-star/20" />
+      <Download size={16} className="shrink-0" />
+      <span className="shrink-0">
+        {t("toast.update.description", { version })}
+      </span>
+      <div className="grow"></div>
       <a
-        className="flex items-center gap-2 text-primary-text hover:underline"
+        className="flex items-center gap-2 text-primary-text hover:underline shrink-0 whitespace-nowrap"
         target="_blank"
         href={href}
       >
-        <ArrowUpRightFromSquare size={18} />
+        <ArrowUpRightFromSquare size={18} className="shrink-0" />
         {t("toast.update.releasePageButton")}
       </a>
-    </div>
+    </ToastContainer>
   );
 }
