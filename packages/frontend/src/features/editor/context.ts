2b206e57f793845460f3657143a36a0a40d80435
Burak Yeniçeri <burak.yn.dev@gmail.com>
2026-04-21 20:27:19 +0300

    refactor(editor): switch to modern tiptap api

--- packages/frontend/src/features/editor/context.ts
+++ packages/frontend/src/features/editor/context.ts
@@ -1,8 +1,8 @@
-import { EditorContent, SlashCommandItem } from "./types";
+import { NoteDTO } from "@darkwrite/common";
 import { Editor } from "@tiptap/core";
 import { createContext } from "react";
-import { NoteDTO } from "@darkwrite/common";
 import { ImageExtensionConfig } from "./extensions/image/image-config";
+import { EditorContent, SlashCommandItem } from "./types";
 
 export interface IDarkwriteEditorContext {
   notes?: NoteDTO[];
