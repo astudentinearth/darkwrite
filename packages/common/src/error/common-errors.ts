ac0d7289345fa39b0947133c9385f878a1995b4d
Burak <burak.yn.dev@gmail.com>
2026-05-08 22:10:18 +0300

    refactor(app): add neverthrow error types

--- packages/common/src/error/common-errors.ts
+++ packages/common/src/error/common-errors.ts
@@ -7,10 +7,12 @@ export type EntityType =
   | "Database"
   | "FileLink";
 
+/** @deprected use neverthrow instead */
 export function _buildNotFoundMessage(entityName: string, entityId: string) {
   return `${entityName} with id ${entityId} not found.`;
 }
 
+/** @deprected use neverthrow instead */
 export class NotFoundError extends ClientError {
   constructor(
     public readonly entityType: EntityType,
@@ -20,24 +22,28 @@ export class NotFoundError extends ClientError {
   }
 }
 
+/** @deprected use neverthrow instead */
 export class IllegalArgumentError extends ClientError {
   constructor(public readonly message: string) {
     super(message);
   }
 }
 
+/** @deprected use neverthrow instead */
 export class InvalidThemeError extends ClientError {
   constructor(public readonly themeId: string) {
     super(`Invalid theme: ${themeId}`);
   }
 }
 
+/** @deprected use neverthrow instead */
 export class InvalidSettingsError extends ClientError {
   constructor(public readonly message: string = "Invalid settings file.") {
     super(message);
   }
 }
 
+/** @deprected use neverthrow instead */
 export class InvalidBackupError extends ClientError {
   constructor(
     public readonly message: string = "This does not seem to be a Darkwrite backup archive.",
@@ -46,8 +52,27 @@ export class InvalidBackupError extends ClientError {
   }
 }
 
+/** @deprected use neverthrow instead */
 export class MutationError extends ClientError {
   constructor(public readonly message: string) {
     super(message);
   }
 }
+
+// everything above is deprecated
+
+export type NoteError =
+  | { type: "note-not-found"; id: string }
+  | { type: "note-failed-to-create"; cause?: unknown }
+  | { type: "note-failed-to-update"; cause?: unknown }
+  | { type: "note-failed-to-move"; cause?: "trashed" }
+  | { type: "note-failed-to-delete"; cause?: unknown };
+
+export type NoteErrorType = NoteError["type"];
+
+export type WorkspaceError =
+  | { type: "workspace-not-found"; id: string }
+  | { type: "workspace-failed-to-create"; cause?: unknown }
+  | { type: "workspace-failed-to-delete"; cause?: unknown };
+
+export type WorkspaceErrorType = WorkspaceError["type"];
