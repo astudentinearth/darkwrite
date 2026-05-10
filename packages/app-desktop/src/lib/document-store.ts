9c106316003b13dbb729dbf98ae0b652d95d0c17
Burak <burak.yn.dev@gmail.com>
2026-05-10 05:39:11 +0300

    refactor(fp): refactor workspace service (8/n)

--- packages/app-desktop/src/lib/document-store.ts
+++ packages/app-desktop/src/lib/document-store.ts
@@ -4,7 +4,10 @@ import { readFile, rm, writeFile } from "fs/promises";
 import { err, ok, Result, ResultAsync } from "neverthrow";
 import path from "node:path";
 
-export type DocumentStoreErr = { type: "path-error" } | fslib.FsError;
+export type DocumentStoreErr =
+  | { type: "path-error" }
+  | { type: "document-not-found"; id: string }
+  | fslib.FsError;
 
 export interface IDocumentStore {
   create: (id: string) => ResultAsync<void, DocumentStoreErr>;
@@ -39,9 +42,18 @@ export function DocumentFileStore(directory: string): IDocumentStore {
   }
 
   function read(id: string) {
-    return getPath(id).asyncAndThen((p) =>
-      fslib.fsResult(readFile(p, "utf-8")),
-    );
+    return getPath(id)
+      .asyncAndThen((path) =>
+        exists(id).andThen((e) =>
+          e
+            ? ok(path)
+            : err({
+                type: "document-not-found",
+                id,
+              } satisfies DocumentStoreErr),
+        ),
+      )
+      .andThen((p) => fslib.fsResult(readFile(p, "utf-8")));
   }
 
   function exists(id: string) {
