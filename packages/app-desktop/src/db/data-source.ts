813ce23edbe12ce9790392dc0b811b77edba367d
Burak Yeniçeri <burak.yn.dev@gmail.com>
2026-05-16 20:38:44 +0300

    fix(db): incorrect file url on windows

--- packages/app-desktop/src/db/data-source.ts
+++ packages/app-desktop/src/db/data-source.ts
@@ -6,17 +6,18 @@ import { randomUUID } from "crypto";
 import { drizzle } from "drizzle-orm/libsql/node";
 import { tmpdir } from "os";
 import { join } from "path";
+import { pathToFileURL } from "url";
 
 const dbPath = DB_PATH;
 export const DB_SYMBOL = Symbol("darkwrite-db-instance");
 
 const getDatabaseUrl = () =>
-  process.env["NODE_ENV"] === "test" ? ":memory:" : `file://${dbPath}`;
+  process.env["NODE_ENV"] === "test" ? ":memory:" : pathToFileURL(dbPath).href;
 
 function makeDrizzleDb(url: string) {
   return drizzle({
     connection: {
-      url,
+      url
     },
     schema: { ...tables, ...relations },
   });
